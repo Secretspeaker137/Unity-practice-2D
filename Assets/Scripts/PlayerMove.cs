41121d51944829844c0527b1132c1c58eb017d02
김은총 <Kimberly137137@gmail.com>
2022-05-17 08:01:47 +0900

    2D 무한 점프하기
    2D 무한 점프하기

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
-
+    public float jumpPower;
     Rigidbody2D rigid; // 물리이동을 위한 변수 선언
     SpriteRenderer spriteRenderer;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>(); // 변수 초기화
         spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+            //Jump
+            if(Input.GetButtonDown("Jump")) { 
+                rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+                anim.SetBool("isJumping", true);
+            }
+
             //Stop Speed
             if(Input.GetButtonDown("Horizontal")) // normalized : 벡터 크기를 1로 만든 상태 (단위벡터)
             { rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y); }
@@ -24,6 +32,12 @@ public class PlayerMove : MonoBehaviour
             //Direction Sprite
             if(Input.GetButtonDown("Horizontal"))
                 spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1; // spriteRenderer.flipX : 조건문
+
+            //Animation
+            if(Mathf.Abs(rigid.velocity.x) < 0.3) // Mathf.Abs() : 절대값
+                anim.SetBool("isWalking", false);
+            else
+                anim.SetBool("isWalking", true);
     }
 
     void FixedUpdate()
@@ -37,5 +51,17 @@ public class PlayerMove : MonoBehaviour
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         else if (rigid.velocity.x < maxSpeed * (-1)) // Left Max Speed 
             rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+        //Landing Platform
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+        if(rayHit.collider != null)
+        {
+            if(rayHit.distance < 0.5f)
+                //Debug.Log(rayHit.collider.name);
+                anim.SetBool("isJumping", false);
+        }
     }
 }
