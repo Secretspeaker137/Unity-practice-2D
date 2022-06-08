28bb638d786810bc5737bc244f44a273b0e1c850
Tiny Cosmos <tinycosmos721@gmail.com>
2022-06-08 20:44:35 +0900

    게임 UI 기초

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,27 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    //public void NextStage()
     public int totalPoint;
     public int stagePoint;
     public int stageIndex;
+
+    //public void HealthDown()
     public int health;
     public PlayerMove player;
+    public GameObject[] Stages;
 
+    //UI
+    public Image[] UIhealth;
+    public Text UIPoint;
+    public Text UIStage;
+    public GameObject UIRestartBtn;
+
+    void Update()
+    {
+        UIPoint.text = (totalPoint + stagePoint).ToString();
+    }
     public void NextStage() // 스테이지가 올라가면 totalPoint에 누적
     {
-        stageIndex++;
+        //Change Stage
+        totalPoint += stagePoint;
+        stagePoint = 0;
 
+        if(stageIndex < Stages.Length)
+        {
+            Stages[stageIndex].SetActive(false);
+            stageIndex++;
+            Stages[stageIndex].SetActive(true);
+            PlayerReposition();
+
+            UIStage.text = "STAGE " + (stageIndex + 1);
+        }
+        else
+        {
+            //Game Clear
+
+            //Player Contol Lock
+            Time.timeScale = 0;
+            //Result UI
+            Debug.Log("게임 클리어!");
+            //Restart Button UI
+            Text btnText = UIRestartBtn.GetComponentInChildren<Text>();
+
+            UIRestartBtn.SetActive(true);
+        }
+     
+        //Calculate Point
         totalPoint += stagePoint;
         stagePoint = 0;
     }
 
     public void HealthDown()
     {
-        if(health > 1)
+        if (health > 1)
+        {
             health--;
+            UIhealth[health].color = new Color(1, 0, 0, 0.4f);
+        }    
         else
         {
             //Player Die Effect
@@ -31,6 +75,7 @@ public class GameManager : MonoBehaviour
             Debug.Log("죽었습니다!");
 
             //Retry Button UI
+            UIRestartBtn.SetActive(true);
 
         }
     }
@@ -38,16 +83,21 @@ public class GameManager : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
-        { 
+        {
             // Player Reposition
-            if(health > 1)
+            if (health > 1)
             {
-                collision.attachedRigidbody.velocity = Vector2.zero;
-                collision.transform.position = new Vector3(0, 0, -1);
+               PlayerReposition();
             }
 
             // Health Down
             HealthDown();
         }
+    }
+
+    void PlayerReposition()
+    {
+        player.transform.position = new Vector3(0, 0, -1);
+        player.VelocityZero();
     }
 }
